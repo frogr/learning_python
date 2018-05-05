427a059d323d6c548d0a4b0b9621f8aa8eaaa2ab
frogr <austindanielfrench@gmail.com>
2018-05-05 01:46:57 -0700

    python hello world done

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -1,1 +1,23 @@
-print('hello world')
+def hello_world():
+    print()
+    print('==%==%==%==%==')
+    print(' HELLO WORLD ! ')
+    print('==%==%==%==%==')
+    print()
+    print('enter "exit" as your name to leave my fun game!')
+    print()
+    greet()
+
+
+def greet():
+    user_input = input('whats ya name? ')
+    if user_input == 'exit':
+        print('goodbye!')
+        exit(0)
+    print('hello', user_input)
+    hello_world()
+
+
+hello_world()
+
+
